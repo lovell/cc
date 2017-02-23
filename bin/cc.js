830e2fb1fe64bbbb3f80dd78135625e353b3ca71
Lovell Fuller <github@lovell.info>
2017-02-23 22:47:46 +0000

    Re-run cc without node_modules in PATH when >2 args Allows compilation of C files via 'cc' alias of gcc

--- bin/cc.js
+++ bin/cc.js
@@ -5,6 +5,12 @@
 const deglob = require('deglob');
 const pkgConf = require('pkg-conf');
 const pythonShell = require('python-shell');
+const childProcess = require('child_process');
+
+if (process.argv.length > 2) {
+  process.env.PATH = process.env.PATH.split(':').slice(2).join(':');
+  process.exit(childProcess.spawnSync('cc', process.argv.slice(2), { stdio: 'inherit' }).status);
+}
 
 const config = pkgConf.sync('cc', {
   defaults: {
